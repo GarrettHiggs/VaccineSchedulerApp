3cd64173be7d81838d785b4aeec3771e73c54366
GarrettHiggs <ghiggs@uw.edu>
2024-02-19 15:19:26 -0800

    Initial

---
+++ vaccine-scheduler-java-main/out/production/vaccine-scheduler-java-main/src/main/resources/create.sql
@@ -0,0 +1,33 @@
+CREATE TABLE Caregivers (
+    Username varchar(255),
+    Salt BINARY(16),
+    Hash BINARY(16),
+    PRIMARY KEY (Username)
+);
+
+CREATE TABLE Availabilities (
+    Time date,
+    Username varchar(255) REFERENCES Caregivers,
+    PRIMARY KEY (Time, Username)
+);
+
+CREATE TABLE Vaccines (
+    Name varchar(255),
+    Doses int,
+    PRIMARY KEY (Name)
+);
+
+CREATE TABLE Patients (
+    Username varchar(255),
+    Salt BINARY(16),
+    Hash BINARY(16),
+    PRIMARY KEY (Username)
+);
+
+CREATE TABLE Appointments (
+    id int PRIMARY KEY,
+    v_name varchar(255) REFERENCES Vaccines(Name),
+    Time date,
+    p_name varchar(255) REFERENCES Patients(Username),
+    c_name varchar(255) REFERENCES Caregivers(Username)
+);
